b8556b0e9695513e4a9afa33625d8873db9b41c0
Mattia Mariselli <mattia.mariselli@gmail.com>
2016-07-27 14:50:37 +0200

    Set revision file optional by configuration

--- classes/executor.js
+++ classes/executor.js
@@ -10,7 +10,7 @@ module.exports = function (gulp, plugins) {
             // rimosso quando manderemo a cagare compass
             const filter = plugins.filter(['**', '!**/*.css'], {restore: true});
             // TODO: remove gulp-compass ASAP and use gulp-sass instead
-            return gulp.src(srcFiles)
+            var promise = gulp.src(srcFiles)
                 .pipe(plugins.plumber())
                 .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.init()))
                 .pipe(plugins.debug())
@@ -21,53 +21,83 @@ module.exports = function (gulp, plugins) {
                 }))
                 .pipe(filter.restore)
                 .pipe(plugins.concat(config.cssPath + outputFilename))
-                .pipe(config.production ? plugins.minifyCss() : plugins.util.noop())
-                .pipe(plugins.rev())
+                .pipe(config.production ? plugins.minifyCss() : plugins.util.noop());
+
+            if (config.revManifest) {
+                promise.pipe(plugins.rev());
+            }
+
+            promise
                 .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.write('.')))
                 .pipe(gulp.dest(config.publicPath))
                 .on('end', function () {
                     // Necessario fino a quando non togliamo compass
                     del.sync('css')
-                })
+                });
 
+            if (config.revManifest) {
                 // Write the rev-manifest.json file for gulp-rev
-                .pipe(plugins.rev.manifest(config.revManifestPath, {
+                promise.pipe(plugins.rev.manifest(config.revManifestPath, {
                     merge: true
-                }))
-                .pipe(gulp.dest('.'));
+                }));
+            }
+
+            promise.pipe(gulp.dest('.'));
+
+            return promise;
         },
         style: function (src, outputFilename, config) {
-            return gulp.src(src)
+            var promise = gulp.src(src)
                 .pipe(plugins.plumber())
                 .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.init()))
                 .pipe(plugins.sassGlob())
                 .pipe(plugins.sass())
                 .pipe(plugins.autoprefixer())
                 .pipe(plugins.concat(config.cssPath + outputFilename))
-                .pipe(config.production ? plugins.minifyCss() : plugins.util.noop())
-                .pipe(plugins.rev())
+                .pipe(config.production ? plugins.minifyCss() : plugins.util.noop());
+
+            if (config.revManifest) {
+                promise.pipe(plugins.rev());
+            }
+
+            promise
                 .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.write('.')))
-                .pipe(gulp.dest(config.publicPath))
-                .pipe(plugins.rev.manifest(config.revManifestPath, {
+                .pipe(gulp.dest(config.publicPath));
+
+            if (config.revManifest) {
+                promise.pipe(plugins.rev.manifest(config.revManifestPath, {
                     merge: true
-                }))
-                .pipe(gulp.dest('.'));
+                }));
+            }
+
+            promise.pipe(gulp.dest('.'));
+
+            return promise;
         },
         script: function (src, outputFilename, config) {
-            return gulp.src(src)
+            var promise = gulp.src(src)
                 .pipe(plugins.plumber())
                 .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.init()))
                 .pipe(plugins.concat(config.jsPath + outputFilename))
-                .pipe(config.production ? plugins.uglify() : plugins.util.noop())
-                .pipe(plugins.rev())
-                .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.write('.')))
-                .pipe(gulp.dest(config.publicPath))
+                .pipe(config.production ? plugins.uglify() : plugins.util.noop());
 
+            if (config.revManifest) {
+                promise.pipe(plugins.rev());
+            }
+
+            promise
+                .pipe(plugins.if(config.sourceMaps, plugins.sourcemaps.write('.')))
+                .pipe(gulp.dest(config.publicPath));
+
+            if (config.revManifest) {
                 // Write the rev-manifest.json file for gulp-rev
-                .pipe(plugins.rev.manifest(config.revManifestPath, {
+                promise.pipe(plugins.rev.manifest(config.revManifestPath, {
                     merge: true
-                }))
-                .pipe(gulp.dest('.'));
+                }));
+            }
+            promise.pipe(gulp.dest('.'));
+
+            return promise;
         },
         copy: function (srcFiles, outputDir, config) {
             return gulp.src(srcFiles)
